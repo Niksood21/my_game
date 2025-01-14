56bd48b0785eba76efb02cf14819022b782a6053
79858 <magsinovfrank@gmail.com>
2025-01-14 23:53:34 +0300

    creating a game window 2.0

--- main.py
+++ main.py
@@ -5,15 +5,38 @@ pygame.init()
 
 width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Игровое окно 800x800")
+pygame.display.set_caption("Наша игра")
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+font = pygame.font.Font(None, 48)
+
+
+def initial_window(text):
+    text_surface = font.render(text, True, black)
+    text_rect = text_surface.get_rect(center=(width // 2, height // 2))
+    screen.blit(text_surface, text_rect)
+
 
 running = True
+game_started = False
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                game_started = True
 
-    screen.fill((255, 255, 255))
+    if not game_started:
+
+        screen.fill(white)
+        initial_window("Нажмите 'Enter', чтобы начать игру")
+    else:
+
+        screen.fill(black)
 
     pygame.display.flip()
 
