ae7ee9bc1d491c7c5111354dbb5f6f4c6c85155c
79858 <magsinovfrank@gmail.com>
2025-01-15 19:43:46 +0300

    description тз

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ black = (0, 0, 0)
 font = pygame.font.Font(None, 48)
 
 
+# background_image = pygame.image.load("D:\projectpygame(game)\background_sky_stain_65935_1440x900.jpg").convert()
+
+
 def initial_window(text):
     text_surface = font.render(text, True, black)
     text_rect = text_surface.get_rect(center=(width // 2, height // 2))
@@ -33,10 +36,10 @@ while running:
     if not game_started:
 
         screen.fill(white)
-        initial_window("Нажмите 'Enter', чтобы начать игру")
+        initial_window("Нажмите Enter, чтобы начать игру")
     else:
-
         screen.fill(black)
+        # screen.blit(background_image, (0, 0))
 
     pygame.display.flip()
 
